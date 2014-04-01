651c0ca4ce520cb2b6ed1d4525c16b05972128d1
CBedo <carlos.bedoy@gmail.com>
2014-04-01 00:26:56 -0600

    Add main view controller

---
+++ Make Good Girls/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Make Good Girls
+//
+//  Created by Carlos Bedoy on 01/04/14.
+//  Copyright (c) 2014 Carlos Bedoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
