a06d701af063fb42bb24eda81081f130629fccbb
CBedo <carlos.bedoy@gmail.com>
2014-04-01 00:56:59 -0600

    Add manager view controller

---
+++ Make Good Girls/ManagerViewController.m
@@ -0,0 +1,34 @@
+//
+//  ManagerViewController.m
+//  Make Good Girls
+//
+//  Created by Carlos Bedoy on 01/04/14.
+//  Copyright (c) 2014 Carlos Bedoy. All rights reserved.
+//
+
+#import "ManagerViewController.h"
+
+@interface ManagerViewController ()
+
+@end
+
+@implementation ManagerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+
+@end
