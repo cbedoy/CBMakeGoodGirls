54e11d54f80e51cedd34b9a743ef2c4460782142
CBedo <carlos.bedoy@gmail.com>
2014-04-03 17:55:54 -0600

    Add new list

---
+++ Make Good Girls/ItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemViewController.h
+//  Make Good Girls
+//
+//  Created by Carlos Bedoy on 03/04/14.
+//  Copyright (c) 2014 Carlos Bedoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemViewController : UIViewController
+
+@end
---
+++ Make Good Girls/ItemViewController.m
@@ -0,0 +1,41 @@
+//
+//  ItemViewController.m
+//  Make Good Girls
+//
+//  Created by Carlos Bedoy on 03/04/14.
+//  Copyright (c) 2014 Carlos Bedoy. All rights reserved.
+//
+
+#import "ItemViewController.h"
+
+@interface ItemViewController ()
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation ItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)actionCancel:(id)sender {
+}
+
+@end
