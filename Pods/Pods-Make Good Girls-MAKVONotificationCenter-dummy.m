5e8c4c79f9b2c7d138ce1f189348b75b7b76b8f0
CBedo <carlos.bedoy@gmail.com>
2014-04-22 00:24:18 -0500

    Added pod file

---
+++ Pods/Pods-Make Good Girls-MAKVONotificationCenter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Make_Good_Girls_MAKVONotificationCenter : NSObject
+@end
+@implementation PodsDummy_Pods_Make_Good_Girls_MAKVONotificationCenter
+@end
---
+++ Pods/Pods-Make Good Girls-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Make_Good_Girls : NSObject
+@end
+@implementation PodsDummy_Pods_Make_Good_Girls
+@end
